fb0d32a6bb140d2af735783071c19d7e78d1f139
pabloarcienega <pabloarcienega@gmail.com>
2021-05-18 22:43:24 -0300

    Proyecto Mercado Liebre de PAblo Arcienega

--- app.js
+++ app.js
@@ -1,14 +1,15 @@
 const express = require('express');
 const path = require('path');
 const app = express();
+const port = process.env.PORT || 3000
 const publicPath = path.resolve(__dirname, './public');
 app.use(express.static(publicPath));
 app.get('/', (req, res) => {
-    console.log(path.resolve(__dirname, './views/home.html'));
+    //console.log(path.resolve(__dirname, './views/home.html'));
     res.sendFile(path.resolve(__dirname, './views/home.html'))
     //res.send('Hola mundo');
 
 })
 
-app.listen(3000, ()=>{console.log('localhost:3000 ya está corriendo')});
+app.listen(3000, ()=>{console.log('Server running en http://localhost:${port}')});
 
